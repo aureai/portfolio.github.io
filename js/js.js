b12a7eab6ebe137cb7f5d319bb9764c7cdb7e53b
aureai <33704538+aureai@users.noreply.github.com>
2021-06-14 11:56:05 -0300

    edit pain

--- js/js.js
+++ js/js.js
@@ -1,12 +1,38 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#white-box").animate({
-            height: '150px', 
-            left: '500px',
-            opacity: '0.7',
-            top: '100px',
-            width: '150px'
-        }, 1500);
-    });
-});
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = ""
+var index = 0, isAdding = true
 
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of the text to be typed using index.
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTyped.length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 2s before playing again
+        setTimeout(function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, 120)
+}
+// start animation
+playAnim()
